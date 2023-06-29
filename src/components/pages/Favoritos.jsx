21e12bd5c95cc7e1d10a26c5275c731202ae7bea
javierFili <javierfiligranaagreda@gmail.com>
2023-06-29 11:11:05 -0400

    union

--- src/components/pages/Favoritos.jsx
+++ src/components/pages/Favoritos.jsx
@@ -4,18 +4,18 @@ import CardFood from "../CardFood";
 function Favoritos() {
   return (
     <div className="card " style={{ padding: "2em", marginTop: "2em" }}>
-    <div className="row">
-      {recetasFav.map((item) => {
-        return (
-          <>
-            <div className="col-sm-12 col-md-5 col-lg-4">
-              <CardFood key={2} title={item.title} src={item.src} />
-            </div>
-          </>
-        );
-      })}
+      <div className="row">
+        {recetasFav.map((item) => {
+          return (
+            <>
+              <div className="col-sm-12 col-md-5 col-lg-4">
+                <CardFood key={2} title={item.title} src={item.src} />
+              </div>
+            </>
+          );
+        })}
+      </div>
     </div>
-  </div>
   );
 }
 
